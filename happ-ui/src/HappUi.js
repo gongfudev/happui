6522a71efb3af387b2089573276fa2a468e74660
Olivier Lange <olange@petit-atelier.ch>
2020-03-25 23:44:38 +0100

    Ajouté code SVG pour engendrer un gradient dans un cercle

--- happ-ui/src/HappUi.js
+++ happ-ui/src/HappUi.js
@@ -1,4 +1,4 @@
-import { html, css, LitElement } from 'lit-element';
+import { html, svg, css, LitElement } from 'lit-element';
 
 export class HappUi extends LitElement {
   static get styles() {
@@ -6,7 +6,7 @@ export class HappUi extends LitElement {
       :host {
         --happ-ui-text-color: #000;
 
-        display: block;
+        display: inline-block;
         padding: 25px;
         color: var(--happ-ui-text-color);
       }
@@ -31,9 +31,23 @@ export class HappUi extends LitElement {
   }
 
   render() {
-    return html`
-      <h2>${this.title} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
-    `;
+    return svg`<svg viewBox="0 0 100 100" aria-label="${this.title}">
+      <defs>
+        <filter id="blur" color-interpolation-filters="linear" x="-50%" y="-50%" width="200%" height="200%">
+          <feGaussianBlur in="SourceGraphic" stdDeviation="9"/>
+        </filter>
+        <mask id="circle">
+          <circle cx="50" cy="50" r="50" fill="white"/>
+        </mask>
+      </defs>
+      <g mask="url(#circle)" filter="url(#blur)">
+        <rect x="-10" width="110" height="110" fill="blue"/>
+        <rect x="50" width="60" height="110" fill="yellow"/>
+        <polygon points="50,50, 60,110, 40,110" fill="#0f8"/>
+        <polygon points="0,0, 100,0, 100,20, 50,50, 0,20" fill="red"/>
+        <polygon points="0,10, 50,50, 0,30" fill="#f0f"/>
+        <polygon points="100,10, 100,30, 50,50" fill="#f80"/>
+      </g>
+    </svg>`;
   }
 }
