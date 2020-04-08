7477a6a0cde75d11a819c9d22723edee2a893e58
Glitch (gfio-happui) <none>
2020-04-08 20:40:15 +0000

    🐸🛎 Checkpoint ./src/HappUi.js:1122723/2593 ./index.html:1122723/18

--- src/HappUi.js
+++ src/HappUi.js
@@ -22,9 +22,9 @@ export class HappUi extends LitElement {
   static get properties() {
     return {
       title: { type: String },
-      sentir: { type: Number },
-      connaitre: { type: Number },
-      comprendre: { type: Number },
+      sentir: { type: Number }, // Value in between 0.0 … 1.0
+      connaitre: { type: Number }, // idem
+      comprendre: { type: Number }, // idem
     };
   }
 
@@ -53,13 +53,8 @@ export class HappUi extends LitElement {
     return svg`<svg viewBox="0 0 100 100" aria-label="${this.title}">
       <title>${this.title}</title>
       <style>
-        .stem {
-          stroke: black;
-          stroke-width: ${this.__width}; }
-        .sprout {
-          stroke: black;
-          stroke-width: ${this.__width};
-          fill-opacity: 0.0; }
+        .stem { stroke: black; stroke-width: ${this.__width}; }
+        .sprout { stroke: black; stroke-width: ${this.__width}; fill-opacity: 0.0; }
       </style>
       <defs>
         <filter id="blur" color-interpolation-filters="linear" x="-50%" y="-50%" width="200%" height="200%">
@@ -69,7 +64,7 @@ export class HappUi extends LitElement {
           <circle cx="50" cy="50" r="50" fill="white"/>
         </mask>
       </defs>
-      <g mask="url(#circle)" filter="url(#blur)">
+      <g id="background-circle" mask="url(#circle)" filter="url(#blur)">
         <rect x="-10" width="110" height="110" fill="blue"/>
         <rect x="50" width="60" height="110" fill="yellow"/>
         <polygon points="50,50, 60,110, 40,110" fill="#0f8"/>
@@ -77,13 +72,19 @@ export class HappUi extends LitElement {
         <polygon points="0,10, 50,50, 0,30" fill="#f0f"/>
         <polygon points="100,10, 100,30, 50,50" fill="#f80"/>
       </g>
-      <g>
-        ${this.__pistils.map(
-          pistil => svg`
-            <g id="${pistil.id}" transform="translate(50.0 50.0) rotate(${pistil.angle} 0 0)">
-              <line id="${pistil.id}" class="stem" x1="0" y1="0" x2="${pistil.length * 50.0 - this.__radius}" y2="0" />
-              <circle id="${pistil.id}" class="sprout" cx="${pistil.length * 50.0}" cy="0" r="${this.__radius}" />
-          `)}
+      <g id="pistils">
+        <g id="p0" transform="translate(50 50) rotate(30 0 0)">
+          <line id="p0:stem" class="stem" x1="0" y1="0" x2="${this.sentir * 47.0 - this.__radius}" y2="0" />
+          <circle id="p0:sprout" class="sprout" cx="${this.sentir * 47.0}" cy="0" r="${this.__radius}" />
+        </g>
+        <g id="p1" transform="translate(50 50) rotate(150 0 0)">
+          <line id="p1:stem" class="stem" x1="0" y1="0" x2="${this.connaitre * 47.0 - this.__radius}" y2="0" />
+          <circle id="p1:sprout" class="sprout" cx="${this.connaitre * 47.0}" cy="0" r="${this.__radius}" />
+        </g>
+        <g id="p2" transform="translate(50 50) rotate(270 0 0)">
+          <line id="p2:stem" class="stem" x1="0" y1="0" x2="${this.comprendre * 47.0 - this.__radius}" y2="0" />
+          <circle id="p2:sprout" class="sprout" cx="${this.comprendre * 47.0}" cy="0" r="${this.__radius}" />
+        </g>
       </g>
     </svg>`;
   }
