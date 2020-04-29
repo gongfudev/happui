450601d5f36c1d535d0c3c08f773d3c611504415
Glitch (gfio-happui) <none>
2020-04-29 20:35:41 +0000

    🍅😄 Checkpoint ./src/HappUi.js:1122723/1608

--- src/HappUi.js
+++ src/HappUi.js
@@ -69,16 +69,15 @@ export class HappUi extends LitElement {
       <style>
         .stem { stroke: white; stroke-width: 3.0; }
         .sprout { stroke: white; stroke-width: 3.0; fill-opacity: 0.0; }
-        .frame { stroke: black; stroke-width: 0.5; fill: none; }
 
-        .stop1 { stop-color: #da00ff; stop-opacity: 25%; }
-        .stop2 { stop-color: #0007d4; stop-opacity: 25%; }
+        .stop1 { stop-color: #da00ff; stop-opacity: 100%; }
+        .stop2 { stop-color: #0007d4; stop-opacity: 100%; }
 
-        .stop3 { stop-color: #00a2ff; stop-opacity: 25%; }
-        .stop4 { stop-color: #00a500; stop-opacity: 25%; }
+        .stop3 { stop-color: #00a2ff; stop-opacity: 100%; }
+        .stop4 { stop-color: #00a500; stop-opacity: 100%; }
 
-        .stop5 { stop-color: #cd0000; stop-opacity: 25%; }
-        .stop6 { stop-color: #ff9000; stop-opacity: 25%; }
+        .stop5 { stop-color: #cd0000; stop-opacity: 100%; }
+        .stop6 { stop-color: #ff9000; stop-opacity: 100%; }
       </style>
       <defs>
         <linearGradient id="gradientSEN">
@@ -102,24 +101,24 @@ export class HappUi extends LitElement {
           <circle cx="50" cy="50" r="50" fill="white"/>
         </mask>
       </defs>
-      <g id="background" mask="url(#circle)" filter="url(#blur)">
-        <rect x="0" y="0" width="100" height="100" fill="white" />
-        <circle id="areaCON" cx="30" cy="50" r="${5+20*this._connaitre}" fill="url(#gradientCON)"/>
-        <circle id="areaSEN" cx="70" cy="50" r="${5+20*this._sentir}" fill="url(#gradientSEN)"/>
-        <circle id="areaCOM" cx="50" cy="30" r="${5+20*this._comprendre}" fill="url(#gradientCOM)"/>
+      <g id="background"  > <!-- filter="url(#blur)" -->
       </g>
-      <g id="pistils">
-        <g id="p0" transform="translate(50 50) rotate(30 0 0)">
-          <line id="p0:stem" class="stem" x1="0" y1="0" x2="${this._sentir * 47 - 5}" y2="0" />
-          <circle id="p0:sprout" class="sprout" cx="${this._sentir * 47}" cy="0" r="5" />
+      <g id="pistils" mask="url(#circle)">
+        <rect x="0" y="0" width="100" height="100" fill="white" stroke="none" />
+        <g id="bSEN" transform="translate(50 50) rotate(30 0 0)">
+          <circle id="bSEN:area" cx="0" cy="50" r="${25+50*this._sentir}" fill="url(#gradientSEN)"/>
+          <line id="bSEN:stem" class="stem" x1="0" y1="0" x2="${this._sentir * 47 - 5}" y2="0" />
+          <circle id="bSEN:sprout" class="sprout" cx="${this._sentir * 47}" cy="0" r="5" />
         </g>
-        <g id="p1" transform="translate(50 50) rotate(150 0 0)">
-          <line id="p1:stem" class="stem" x1="0" y1="0" x2="${this._connaitre * 47 - 5}" y2="0" />
-          <circle id="p1:sprout" class="sprout" cx="${this._connaitre * 47}" cy="0" r="5" />
+        <g id="bCON" transform="translate(50 50) rotate(150 0 0)">
+          <circle id="bCON:area" cx="0" cy="50" r="${10+25*this._comprendre}" fill="url(#gradientCON)"/>
+          <line id="bCON:stem" class="stem" x1="0" y1="0" x2="${this._connaitre * 47 - 5}" y2="0" />
+          <circle id="bCON:sprout" class="sprout" cx="${this._connaitre * 47}" cy="0" r="5" />
         </g>
-        <g id="p2" transform="translate(50 50) rotate(270 0 0)">
-          <line id="p2:stem" class="stem" x1="0" y1="0" x2="${this._comprendre * 47 - 5}" y2="0" />
-          <circle id="p2:sprout" class="sprout" cx="${this._comprendre * 47}" cy="0" r="5" />
+        <g id="bCOM" transform="translate(50 50) rotate(270 0 0)">
+          <circle id="bCOM:area" cx="0" cy="50" r="${10+25*this._connaitre}" fill="url(#gradientCOM)"/>
+          <line id="bCOM:stem" class="stem" x1="0" y1="0" x2="${this._comprendre * 47 - 5}" y2="0" />
+          <circle id="bCOM:sprout" class="sprout" cx="${this._comprendre * 47}" cy="0" r="5" />
         </g>
       </g>
     </svg>`;
