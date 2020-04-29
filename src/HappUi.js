6e8b37198f28d35537ccc4d4d8ae8c6618fa30c4
Glitch (gfio-happui) <none>
2020-04-29 19:53:40 +0000

    Import of gongfuio/happui

--- src/HappUi.js
+++ src/HappUi.js
@@ -1,5 +1,21 @@
 import { html, svg, css, LitElement } from 'lit-element';
 
+// from http://www.petercollingridge.co.uk/tutorials/svg/interactive/dragging/
+// and https://github.com/petercollingridge/code-for-blog/blob/36ba73c7b763022731a72813249cdc56e7dba8c0/svg-interaction/draggable/draggable_groups.svg?short_path=be4270d
+
+function toRadians(degrees) {
+  return degrees * Math.PI / 180
+}
+
+function displacement(pt1, pt2) {
+  return {x: pt2.x - pt1.x, y: pt2.y - pt1.y}
+}
+
+function projection(pt, angledeg) {
+  let a = toRadians(angledeg)
+  return pt.x * Math.cos(a) + pt.y * Math.sin(a)
+}          
+
 // Utility functions
 function clamp( val, min, max) {
   return val <= min ? min : val >= max ? max : val;
@@ -37,13 +53,15 @@ export class HappUi extends LitElement {
     this._connaitre = 0.75;
     this._comprendre = 0.75;
 
-    this._sentir_incr = 0.1;
-    this._connaitre_incr = 0.1;
-    this._comprendre_incr = 0.1;
-
     // Private properties
     this.__max = 0.90; // Maximum value of the properties { sentir, connaitre, comprendre }
     this.__min = 0.10; // Minimum value … idem …
+
+    this._sentir_angle = 30
+    this._connaitre_angle = 150
+    this._comprendre_angle = 270
+
+    this.pistil_names = {'p0:sprout': 'sentir', 'p1:sprout': 'connaitre', 'p2:sprout': 'comprendre'}
   }
 
   get sentir() { return this._sentir; }  
@@ -67,25 +85,59 @@ export class HappUi extends LitElement {
     this.requestUpdate( 'comprendre', oldVal);
   }
 
-  _handleClick(evt) {
-    const pistils = {'p0:sprout': 'sentir', 'p1:sprout': 'connaitre', 'p2:sprout': 'comprendre'}
-    let pistil = pistils[evt.target.id]
-    if (pistil) {
-      this[pistil] += this[`_${pistil}_incr`]
-      if (this[pistil] >= this.__max) {
-        this[pistil] = this.__max 
-        this[`_${pistil}_incr`] *= -1
-      } else if (this[pistil] <= this.__min) {
-        this[pistil] = this.__min 
-        this[`_${pistil}_incr`] *= -1
-      }
+  _getMousePosition(evt) {
+    let svg = evt.currentTarget
+    let CTM = svg.getScreenCTM();
+    let pt = svg.createSVGPoint();
+    pt.x = evt.clientX; pt.y = evt.clientY;
+    pt = pt.matrixTransform(CTM.inverse());
+    return pt
+  }
+
+  _mousedown(evt) {
+    let pistil_name = this.pistil_names[evt.target.id]
+    if (pistil_name) {
+      //console.log('_mousedown startDrag event', evt.target.id) 
+      let mousePos = this._getMousePosition(evt)
+      this.offset = mousePos
+      this.isDragging = true
     }
   }
 
+  _mousemove(evt) {
+    if (! this.isDragging) {
+      return
+    }
+    let pistil_name = this.pistil_names[evt.target.id]
+    if (pistil_name) {
+      //console.log('_mousemove drag event', evt.target.id) 
+      let mousePos = this._getMousePosition(evt)
+      let displ = displacement(this.offset, mousePos)
+      let proj = projection(displ, this[`_${pistil_name}_angle`])
+      this.offset = mousePos
+      const sprout_length = 47.0
+      this[pistil_name] += proj / sprout_length
+    }
+  }
+
+  _mouseup(event) {
+    // console.log('_mouseup endDrag', event)
+    this.isDragging = false
+  }
+
+  _mouseleave(event) {
+    // console.log('_mouseleave endDrag', event)
+    this.isDragging = false
+  }
+
   render() {
-    return svg`<svg viewBox="0 0 100 100""
+    return svg`<svg viewBox="0 0 100 100"
         aria-label="${this.title}"
         @click="${this._handleClick}"
+        @mousedown="${this._mousedown}"
+        @mousemove="${this._mousemove}"
+        @mouseup="${this._mouseup}"
+        @mouseleave_="${this._mouseleave}"
       >
       <title>${this.title}</title>
       <style>
