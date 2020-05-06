7a883d9aaa4825887da8eb5d3109dc92a7f3da28
Glitch (gfio-happui) <none>
2020-05-06 19:59:08 +0000

    🏃🎃 Checkpoint ./index.html:1122723/54 ./src/Slider.js:1122723/9715 ./slider.js:1122723/143 ./src/HappUiSlider.js:1122723/29 ./happ-ui-slider.js:1122723/114 ./src/HappUi.js:1122723/7

---
+++ happ-ui-slider.js
@@ -0,0 +1,3 @@
+import { Slider } from './src/HappUiSlider.js';
+
+window.customElements.define('happ-ui-slider', Slider);
---
+++ src/HappUiSlider.js
@@ -0,0 +1,34 @@
+import { html, css, LitElement } from 'lit-element';
+
+// Web Component definition
+export class Slider extends LitElement {
+  static get styles() {
+    return css`
+      :host {
+        --happ-ui-text-color: #000;
+
+        display: inline-block;
+        padding: 0; margin: 0;
+        color: var(--happ-ui-text-color);
+      }
+    `;
+  }
+
+  static get properties() {
+    return {
+      value: { type: Number }, // Value in between 0.0 … 1.0
+    };
+  }
+
+  constructor() {
+    super();
+    
+    // Observed properties
+    this.value = 0.5;
+  }
+
+  render() {    
+    return html`<input type="range" min="0.0" max="1.0" step="0.1" value="${this.value}">
+      <input type="number" step="0.5" value="${this.value}">`;
+  }
+}
