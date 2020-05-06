209f941a26115f4d8b551db5b62dc13fc368099d
Glitch (gfio-happui) <none>
2020-05-06 21:16:24 +0000

    🏕🎡 Updated with Glitch  / Hack Session #418

--- happ-ui.js
+++ happ-ui.js
@@ -1,3 +1,5 @@
 import { HappUi } from './src/HappUi.js';
+import { Slider } from './src/HappUiSlider.js';
 
+window.customElements.define('happ-ui-slider', Slider);
 window.customElements.define('happ-ui', HappUi);
